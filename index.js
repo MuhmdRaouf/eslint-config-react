67f4f29291cbcb8185b62285a3606b0f12316bb2
Mohammed Abdel Raouf <mohammed@raouf.me>
2021-07-23 13:29:49 +0200

    perf: put back children validation to prop-types

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-commonjs, no-undef
 module.exports = {
   extends: '@muhmdraouf/eslint-config',
-  plugins: ['react', 'react-hooks'],
-  rules: { 'react/prop-types': ['error', { ignore: ['children'] }] },
+  plugins: ['react', 'react-hooks']
 };
