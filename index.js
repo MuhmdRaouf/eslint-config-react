f0f923adaf620e078aa1861fb2bd0057e9b2a385
Mohammed Abdel Raouf <mohammed@raouf.me>
2021-07-23 14:33:57 +0200

    perf: pump up @muhmdraouf/eslint-config version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-commonjs, no-undef
 module.exports = {
   extends: '@muhmdraouf/eslint-config',
-  plugins: ['react', 'react-hooks']
+  plugins: ['react', 'react-hooks'],
 };
